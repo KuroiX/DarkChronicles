45846cd202caee3ef6d5ae290a66995520cbd640
Ignacio Pizarro <ignaciopigar@gmail.com>
2020-01-08 15:44:43 +0100

    Finally done some work. Sprites optimized 1%

--- DarkChronicles/Assets/Creator Kit - RPG/Scripts/Gameplay/PlayerShooting.cs
+++ DarkChronicles/Assets/Creator Kit - RPG/Scripts/Gameplay/PlayerShooting.cs
@@ -6,28 +6,37 @@ public class PlayerShooting : MonoBehaviour
 {
     public GameObject projectile;
     public GameObject player;
-
+    public float cooldown;
     public float projectileSpeed;
-    private Vector3 target;
+    public float offset;
+    
+    private float remainingCooldown;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
-        Vector3 difference = target - player.transform.position;
-        float rotationZ = Mathf.Atan2(difference.y, difference.x) + Mathf.Rad2Deg;
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - player.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
-        if (Input.GetMouseButton(0))
+
+        if (remainingCooldown <= 0)
         {
-            float distance = difference.magnitude;
-            Vector2 direction = difference / distance;
-            direction.Normalize();
-            sendProjectile(direction, rotationZ);
+            if (Input.GetMouseButton(0))
+            {
+                float distance = difference.magnitude;
+                Vector2 direction = difference / distance;
+                sendProjectile(direction.normalized, rotationZ);
+                remainingCooldown = cooldown;
+            }
+        }
+        else
+        {
+            remainingCooldown -= Time.deltaTime;
         }
     }
 
@@ -35,7 +44,7 @@ public class PlayerShooting : MonoBehaviour
     {
         GameObject p = Instantiate(projectile) as GameObject;
         p.transform.position = player.transform.position;
-        p.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
+        p.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ + offset);
         p.GetComponent<Rigidbody2D>().velocity = direction * projectileSpeed;
     }
 }
