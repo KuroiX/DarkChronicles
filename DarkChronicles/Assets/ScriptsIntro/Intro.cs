8495e62d33a4896f3eb3a6eebb18af939e52bbd7
Carolina <carolina.kawabata@gmail.com>
2020-01-17 18:31:04 +0100

    Intro

---
+++ DarkChronicles/Assets/ScriptsIntro/Intro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    public List<String> nextText;
+
+    public Text text;
+    
+    private int index = 0;
+    
+    private void Awake()
+    {
+        PlayNext();
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            PlayNext();
+        }
+    }
+
+    public void PlayNext()
+    {
+        if (index < nextText.Count)
+        {
+            text.text = nextText[index];
+            playableDirector.Play();
+            index++;
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+    }
+}
