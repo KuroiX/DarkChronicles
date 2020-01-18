5a0d1338e4174bfaa3da34ce97a84f21dcdb722d
Carolina <carolina.kawabata@gmail.com>
2020-01-18 22:26:59 +0100

    added songs as mp3. Intro is done. Npcs with dialogues in the scene.

---
+++ DarkChronicles/Assets/ScriptsIntro/AudioFadeOut.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class AudioFadeOut: MonoBehaviour {
+ 
+    public static IEnumerator FadeOut (AudioSource audioSource, float FadeTime)
+    {
+
+        float currentVolume = audioSource.volume;
+        while (audioSource.volume > 0) {
+            audioSource.volume -= currentVolume * Time.deltaTime / FadeTime;
+ 
+            yield return null;
+        }
+ 
+        audioSource.Stop ();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+
+    }
+ 
+}
