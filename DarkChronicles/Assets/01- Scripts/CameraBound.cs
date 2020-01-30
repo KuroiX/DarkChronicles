1bfc9d3a4f5f3b162e5a85119e04caba99f3bc1c
KuroiX <julian.geheeb@gmx.de>
2020-01-30 13:15:35 +0100

    camera boundaries + no collision with water in village

---
+++ DarkChronicles/Assets/01- Scripts/CameraBound.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBound : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 xAxis;
+    [SerializeField]
+    private Vector2 yAxis;
+    
+    void Update()
+    {
+        SetCameraPosition();
+    }
+
+    void SetCameraPosition()
+    {
+        Vector3 current = transform.position;
+
+        if (current.x < xAxis.x)
+        {
+            if (current.y < yAxis.x)
+            {
+                transform.position = new Vector3(xAxis.x, yAxis.x, current.z);
+            } 
+            else if (current.y > yAxis.y)
+            {
+                transform.position = new Vector3(xAxis.x, yAxis.y, current.z);
+            }
+            else
+            {
+                transform.position = new Vector3(xAxis.x, current.y, current.z);
+            }
+        }
+        else if (current.x > xAxis.y)
+        {
+            if (current.y < yAxis.x)
+            {
+                transform.position = new Vector3(xAxis.y, yAxis.x, current.z);
+            } 
+            else if (current.y > yAxis.y)
+            {
+                transform.position = new Vector3(xAxis.y, yAxis.y, current.z);
+            }
+            else
+            {
+                transform.position = new Vector3(xAxis.y, current.y, current.z);
+            }
+        }
+        else
+        {
+            if (current.y < yAxis.x)
+            {
+                transform.position = new Vector3(current.x, yAxis.x, current.z);
+            } 
+            else if (current.y > yAxis.y)
+            {
+                transform.position = new Vector3(current.x, yAxis.y, current.z);
+            }
+        }
+    }
+}
