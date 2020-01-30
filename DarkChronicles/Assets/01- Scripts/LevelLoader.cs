44f865230560bbe07e8fc588c169c5a9508f1f98
KuroiX <julian.geheeb@gmx.de>
2020-01-30 18:23:14 +0100

    sorry i pushed in master -> levelloader

---
+++ DarkChronicles/Assets/01- Scripts/LevelLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public static LevelLoader manager;
+
+    void Start()
+    {
+        manager = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void LoadScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
---
+++ DarkChronicles/Assets/01- Scripts/LoadTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTrigger : MonoBehaviour
+{
+    [SerializeField] private int levelIndex;
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag.Equals("Player"))
+        {
+            LevelLoader.manager.LoadScene(levelIndex);
+        }
+    }
+}
