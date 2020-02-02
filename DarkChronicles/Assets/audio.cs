cccfa94c388ef28e2ae4c741aaad6da313f258b1
KuroiX <julian.geheeb@gmx.de>
2020-02-02 23:42:49 +0100

    more bugs but works

---
+++ DarkChronicles/Assets/audio.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+    public void PlaySound(int index)
+    {
+        AudioManager.Manager.PlaySound(index);
+    }
+    
+    public void PlayMusic(int index)
+    {
+        AudioManager.Manager.PlayMusic(index);
+    }
+}
